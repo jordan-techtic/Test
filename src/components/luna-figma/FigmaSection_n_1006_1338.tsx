56f158b2f0aaa6d0eb069802441d7ef430f06d9f
Luna Agent <luna@jordan.local>
2026-09-25 11:46:56 +0530

    fix(frontend): keep owned layout before self-review

--- src/components/luna-figma/FigmaSection_n_1006_1338.tsx
+++ src/components/luna-figma/FigmaSection_n_1006_1338.tsx
@@ -1,9 +1,9 @@
 /** luna-spec-codegen: owned-layout */
 export function FigmaSection_n_1006_1338() {
   return (
-    <section data-figma-node="1006:1338" className="absolute box-border left-[324px] top-[145px] w-[403.04px] h-[403.04px] opacity-[0.3] block">
-      <div className="relative box-border w-[403.04px] h-[403.04px] block bg-[#c8a47e]">
-        <div data-figma-node="1006:1339" className="box-border w-[403.04px] h-[403.04px] absolute left-[0px] top-[0px] bg-[#c8a47e]"></div>
+    <section data-figma-node="1006:1338" className="absolute box-border left-[324px] top-[145px] w-[403px] h-[403px] opacity-[0.3] block">
+      <div className="relative box-border w-[403px] h-[403px] block bg-[#c8a47e]">
+        <div data-figma-node="1006:1339" className="box-border w-[403px] h-[403px] absolute left-[0px] top-[0px] bg-[#c8a47e]"></div>
       </div>
     </section>
   );
